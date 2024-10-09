d3f51bd92a16bb0d98ffa3b4aa0b68e868b323f8
ACER <210220@softwarica.edu.np>
2024-10-09 21:58:30 +0545

    token and auth full w logout

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import Login from "./pages/login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
@@ -6,13 +6,22 @@ import MyBlogs from "./pages/MyBlogs";
 import Profile from "./pages/Profile";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import { useEffect, useState } from "react";
 
+import {auth} from './firebase';
 
 function App() {
+
+    const [user,setUser]=useState();
+    useEffect(()=>{
+        auth.onAuthStateChanged((user)=>{
+            setUser(user);
+        });
+    },[])
 return (
 <BrowserRouter>
 <Routes>
-<Route path="/" element={<Login/>}/>
+<Route path="/" element={user ? <Navigate to ="/Home"/> : <Login/>}/>
 <Route path="/Register" element={<Register/>}/>
 <Route path="/Home" element={<Home/>}/>
 <Route path="/MyBlogs" element={<MyBlogs/>}/>
