9d217430dd14ec71f12cfeb8a1395509955af401
ACER <210220@softwarica.edu.np>
2024-10-10 18:05:09 +0545

    responsive

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,74 +4,95 @@ import { Link } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-
-import {auth,db} from "../firebase";
+import { auth, db } from "../firebase";
 import { createUserWithEmailAndPassword } from 'firebase/auth';
-import { setDoc,doc } from 'firebase/firestore';
+import { setDoc, doc } from 'firebase/firestore';
 
 const Register = () => {
+const [fname, setFname] = useState('');
+const [email, setEmail] = useState('');
+const [username, setUsername] = useState('');
+const [password, setPassword] = useState('');
 
-    const[fname, setFname]=useState('');
-    const[email, setEmail]=useState('');
-    const[username, setUsername]=useState('');
-    const[password, setPassword]=useState('');
-
-    const handleRegister=  async(e)=>{
-        e.preventDefault();
-        try{
-        await createUserWithEmailAndPassword(auth, email,password);
-        const user = auth.currentUser;
-            console.log(user);
-            if(user){
-                await setDoc(doc(db, "Users", user.uid),{
-                    firstname: fname,
-                    userName:username,
-                    email: user.email,
-                    passWord: password
-                });
-            }
-            toast.success('Successfully registerred!!!', {position:"top-center",})
-        }
-        catch(error){
-            toast.error(error.message, {position:"bottom-center",})
-
-        }
-}
+const handleRegister = async (e) => {
+    e.preventDefault();
+    try {
+    await createUserWithEmailAndPassword(auth, email, password);
+    const user = auth.currentUser;
+    console.log(user);
+    if (user) {
+        await setDoc(doc(db, "Users", user.uid), {
+        firstname: fname,
+        userName: username,
+        email: user.email,
+        passWord: password
+        });
+    }
+    toast.success('Successfully registered!!!', { position: "top-center" });
+    } catch (error) {
+    toast.error(error.message, { position: "bottom-center" });
+    }
+};
 
 return (
-<div className='Maincontainer  h-[100vh] w-full relative '>
-    <div className="register  h-[100vh] w-full relative">
-    <img src='../images/authbg.jpg' className='h-[100vh] w-full ' alt='bgpic'/>
+    <div className='Maincontainer h-full w-full relative'>
+    <div className="register h-full w-full relative flex justify-center items-center">
+        <img src='../images/authbg.jpg' className='h-full w-full object-cover' alt='bgpic' />
 
-    <Typewriter/>
+        <Typewriter />
 
-    <form onSubmit={handleRegister}
-    className="registercontainer h-[600px] w-[420px] bg-slate-500 absolute top-[100px] right-[200px] rounded-xl ">
-    <img src='../images/containerbg.jpg' className='h-[100%] w-full absolute inset-0 rounded-xl' alt='containerbg'/>
+        <form onSubmit={handleRegister}
+        className="registercontainer w-[90%] md:w-[420px] bg-slate-500 absolute md:top-[100px] md:right-[200px] top-0 right-0 left-0 bottom-0 m-auto p-6 rounded-xl"
+        style={{ maxWidth: '90%', maxHeight: '600px' }}
+        >
+        <img src='../images/containerbg.jpg' className='absolute inset-0 h-full w-full object-cover rounded-xl' alt='containerbg' />
 
-        <div className="inputs flex flex-col justify-center items-center relative h-full w-full">
-        <input onChange={(e)=>setFname(e.target.value)} value={fname}
-        type="text" placeholder='First name'  className='inputfield w-[300px] h-[50px] rounded-xl p-4 mt-10'/>
+        <div className="inputs flex flex-col justify-center items-center relative h-full w-full z-10">
+            <input
+            onChange={(e) => setFname(e.target.value)} value={fname}
+            type="text"
+            placeholder='First name'
+            className='inputfield w-full md:w-[300px] h-[50px] rounded-xl p-4 mt-6 bg-white shadow-lg'
+            />
 
-        <input onChange={(e)=>setEmail(e.target.value)} value={email}
-        type="email" placeholder='Email'  className='inputfield w-[300px] h-[50px] rounded-xl p-4 mt-10'/>
+            <input
+            onChange={(e) => setEmail(e.target.value)} value={email}
+            type="email"
+            placeholder='Email'
+            className='inputfield w-full md:w-[300px] h-[50px] rounded-xl p-4 mt-4 bg-white shadow-lg'
+            />
 
-        <input onChange={(e)=>setUsername(e.target.value)} value={username}
-        type="text" placeholder='Username' className='inputfield w-[300px] h-[50px] rounded-xl p-4 mt-12'/>
+            <input
+            onChange={(e) => setUsername(e.target.value)} value={username}
+            type="text"
+            placeholder='Username'
+            className='inputfield w-full md:w-[300px] h-[50px] rounded-xl p-4 mt-4 bg-white shadow-lg'
+            />
 
-        <input onChange={(e)=>setPassword(e.target.value)} value={password}
-        type="password" placeholder='Password'  className='inputfield w-[300px] h-[50px] rounded-xl p-4 mt-10'/>
+            <input
+            onChange={(e) => setPassword(e.target.value)} value={password}
+            type="password"
+            placeholder='Password'
+            className='inputfield w-full md:w-[300px] h-[50px] rounded-xl p-4 mt-4 bg-white shadow-lg'
+            />
 
-        <button  type='submit' className='bg-bgdeepTurquoise h-[50px]  w-[100px] rounded-xl text-orange font-sans mt-10'>Submit</button>
-        <Link to="/"> <h2 className='underline font-sans text-orange text-sm md:text-lg cursor-pointer mt-5'>Login</h2></Link>
+            <button
+            type='submit'
+            className='bg-bgdeepTurquoise h-[50px] w-[150px] md:w-[100px] rounded-xl text-orange font-sans mt-8'
+            >
+            Submit
+            </button>
+
+            <Link to="/">
+            <h2 className='underline font-sans text-orange text-sm md:text-lg cursor-pointer mt-5'>
+                Login
+            </h2>
+            </Link>
         </div>
-
-
-    </form>
-        </div>
-
+        </form>
     </div>
-)
+    </div>
+);
 }
 
 export default Register;
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,58 +1,75 @@
 import React, { useState } from 'react'
 import Typewriter from '../components/Typerwriter';
 import { Link } from 'react-router-dom';
-import {toast } from 'react-toastify';
+import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
 
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../firebase';
 
-
-
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-    const [email,setEmail]=useState('');
-    const [password,setPassword]=useState('');
-
-    const handlelogin= async(e)=>{
-        e.preventDefault();
-        try{
-            await signInWithEmailAndPassword(auth,email,password);
-            window.location.href='/Home';
-            toast.success('Login successful', {position:"top-center",})
-
-        }
-        catch(error){
-            toast.success(error.message, {position:"top-center",})
-
-        }
+  const handlelogin = async (e) => {
+    e.preventDefault();
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      window.location.href = '/Home';
+      toast.success('Login successful', { position: "top-center" });
+    } catch (error) {
+      toast.error(error.message, { position: "top-center" });
     }
-return (
-    <div className='Maincontainer  h-[100vh] w-full relative '>
-    <div className="login  h-[100vh] w-full relative">
-    <img src='../images/authbg.jpg' className='h-[100vh] w-full ' alt='bgpic'/>
+  };
 
-    <Typewriter/>
+  return (
+    <div className='Maincontainer h-full w-full relative'>
+      <div className="login h-full w-full relative flex justify-center items-center">
+        <img src='../images/authbg.jpg' className='h-full w-full object-cover' alt='bgpic' />
 
-    <form onSubmit={handlelogin}
-    className="logincontainer h-[600px] w-[420px] bg-slate-500 absolute top-[100px] right-[200px] rounded-xl ">
-    <img src='../images/containerbg.jpg' className='h-[100%] w-full absolute inset-0 rounded-xl' alt='containerbg'/>
+        <Typewriter />
 
-        <div className="inputs flex flex-col justify-center items-center relative h-full w-full">
-            <input type="email" onChange={(e)=>setEmail(e.target.value)} value={email}
-            placeholder='Email' className='inputfield w-[300px] h-[50px] rounded-xl p-4 mt-12'/>
+        <form
+          onSubmit={handlelogin}
+          className="logincontainer w-[90%] md:w-[420px] bg-slate-500 absolute md:top-[100px] md:right-[200px] top-0 right-0 left-0 bottom-0 m-auto p-6 rounded-xl"
+          style={{ maxWidth: '90%', maxHeight: '600px' }}
+        >
+          <img src='../images/containerbg.jpg' className='absolute inset-0 h-full w-full object-cover rounded-xl' alt='containerbg' />
 
-            <input type="password" onChange={(e)=>setPassword(e.target.value)} value={password}
-            placeholder='Password'  className='inputfield w-[300px] h-[50px] rounded-xl p-4 mt-10'/>
+          <div className="inputs flex flex-col justify-center items-center relative h-full w-full z-10">
+            <input
+              type="email"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              placeholder='Email'
+              className='inputfield w-full md:w-[300px] h-[50px] rounded-xl p-4 mt-12 bg-white shadow-lg'
+            />
 
-            <button type='submit' className='bg-bgdeepTurquoise h-[50px]  w-[100px] rounded-xl text-orange font-sans mt-10'>Login</button>
-            <Link to="/Register"> <h2 className='underline font-sans text-orange text-sm md:text-lg cursor-pointer mt-5'>Register</h2></Link>
-        </div>
-    </form>
-</div>
+            <input
+              type="password"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              placeholder='Password'
+              className='inputfield w-full md:w-[300px] h-[50px] rounded-xl p-4 mt-4 bg-white shadow-lg'
+            />
+
+            <button
+              type='submit'
+              className='bg-bgdeepTurquoise h-[50px] w-[150px] md:w-[100px] rounded-xl text-orange font-sans mt-8'
+            >
+              Login
+            </button>
+
+            <Link to="/Register">
+              <h2 className='underline font-sans text-orange text-sm md:text-lg cursor-pointer mt-5'>
+                Register
+              </h2>
+            </Link>
+          </div>
+        </form>
+      </div>
     </div>
-)
+  );
 }
 
 export default Login;
